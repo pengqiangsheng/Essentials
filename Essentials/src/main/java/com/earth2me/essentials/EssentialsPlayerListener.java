77dff33904f5d50c2e60b1947413c4a82e73858e
pengqiangsheng <2945802136@qq.com>
2025-05-21 22:18:39 +0800

    fixed: joinmessage 2

--- Essentials/src/main/java/com/earth2me/essentials/EssentialsPlayerListener.java
+++ Essentials/src/main/java/com/earth2me/essentials/EssentialsPlayerListener.java
@@ -329,7 +329,7 @@ public class EssentialsPlayerListener implements Listener, FakeAccessor {
         final String joinMessage = event.getJoinMessage();
         ess.runTaskAsynchronously(() -> delayedJoin(event.getPlayer(), joinMessage));
 
-        if (hideJoinQuitMessages() || ess.getSettings().allowSilentJoinQuit() || !ess.getSettings().isCustomJoinMessage()) {
+        if (hideJoinQuitMessages() || ess.getSettings().allowSilentJoinQuit() || ess.getSettings().isCustomJoinMessage()) {
             event.setJoinMessage(null);
         }
     }
@@ -401,7 +401,7 @@ public class EssentialsPlayerListener implements Listener, FakeAccessor {
                         user.setVanished(true);
                     }
                     effectiveMessage = null;
-                } else if (message == null || hideJoinQuitMessages()) {
+                } else if (hideJoinQuitMessages()) {
                     ess.getLogger().info(message);
                     if(hideJoinQuitMessages()) {
                         ess.getLogger().info("hideJoinQuitMessages:true");
